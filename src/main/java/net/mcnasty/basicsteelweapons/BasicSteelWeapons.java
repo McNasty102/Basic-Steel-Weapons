71b5fe68284aa2c6da91b378c8d2270b2591bcc8
RayTM02 <raymondtmcnaughtoniii@gmail.com>
2026-01-11 09:18:57 -0500

    Attempting to use BW as dependency

--- src/main/java/net/mcnasty/basicsteelweapons/BasicSteelWeapons.java
+++ src/main/java/net/mcnasty/basicsteelweapons/BasicSteelWeapons.java
@@ -33,7 +33,7 @@ import org.slf4j.Logger;
 public class BasicSteelWeapons
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "basic-steel-weapons";
+    public static final String MODID = "basicsteelweapons";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
